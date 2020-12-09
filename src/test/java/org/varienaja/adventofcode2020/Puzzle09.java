7d04cbc9e56c94cba19ebdd374a4076a3330bb0f
varienaja <varienaja@mercury>
2020-12-09 06:33:45 +0100

    Day 10 2020

--- src/test/java/org/varienaja/adventofcode2020/Puzzle09.java
+++ src/test/java/org/varienaja/adventofcode2020/Puzzle09.java
@@ -2,8 +2,9 @@ package org.varienaja.adventofcode2020;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.stream.LongStream;
 
 import org.junit.Test;
 
@@ -15,31 +16,69 @@ import org.junit.Test;
  */
 public class Puzzle09 extends PuzzleAbs {
 
-  private long solveA(List<String> lines) {
-    int result = 0;
-    return result;
+  private long solveA(long[] lines, int preamble) {
+    for (int i = preamble; i < lines.length; i++) {
+      boolean matchFound = false;
+      next: for (int j = i - preamble; j < i - 1; j++) {
+        for (int k = j + 1; k < i; k++) {
+          if (lines[j] + lines[k] == lines[i]) {
+            matchFound = true;
+            break next;
+          }
+        }
+      }
+      if (!matchFound) {
+        return lines[i];
+      }
+    }
+
+    return -1;
   }
 
-  private long solveB(List<String> lines) {
-    int result = 0;
-    return result;
+  private long solveB(long[] lines, long toFind) {
+    int start = 0;
+    int end = 0;
+    int sum = 0;
+    boolean found = false;
+    while (!found) {
+      while (sum < toFind) {
+        sum += lines[end];
+        end++;
+      }
+      if (sum == toFind) {
+        long[] matching = new long[end - start];
+        System.arraycopy(lines, start, matching, 0, end - start);
+        LongSummaryStatistics lss = LongStream.of(matching).summaryStatistics();
+        return lss.getMin() + lss.getMax();
+      }
+      if (sum > toFind) {
+        start++;
+        end = start;
+        sum = 0;
+      }
+    }
+
+    return -1;
   }
 
   @Test
   public void testDay09() {
-    List<String> input = Arrays.asList( //
-    );
-    assertEquals(0, solveA(input));
+    long[] input = {
+        35L, 20L, 15L, 25L, 47L, 40L, 62L, 55L, 65L, 95L, 102L, 117L, 150L, 182L, 127L, 219L, 299L, 277L, 309L, 576L
+    };
+    assertEquals(127, solveA(input, 5));
 
     announceResultA();
     List<String> lines = getInput();
-    long result = solveA(lines);
-    // assertEquals(0L, result);
+    long[] numbers = lines.stream().map(Long::parseLong).mapToLong(l -> l).toArray();
+    long result = solveA(numbers, 25);
+    assertEquals(36845998L, result);
     System.out.println(result);
 
     announceResultB();
-    result = solveB(lines);
-    // assertEquals(0L, result);
+    assertEquals(62, solveB(input, 127));
+    result = solveB(numbers, result);
+    assertEquals(4830226L, result);
     System.out.println(result);
   }
 

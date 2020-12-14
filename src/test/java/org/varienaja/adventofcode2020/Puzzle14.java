ed292f7c9fc79176c4af52df50d7ef4400287f4e
varienaja <varienaja@mercury>
2020-12-14 17:42:15 +0100

    Extracted methods

--- src/test/java/org/varienaja/adventofcode2020/Puzzle14.java
+++ src/test/java/org/varienaja/adventofcode2020/Puzzle14.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.junit.Test;
 
@@ -18,61 +19,49 @@ import org.junit.Test;
  */
 public class Puzzle14 extends PuzzleAbs {
 
-  private List<Long> mask(long value, String mask, char exceptionChar) {
-    char[] masked = new char[mask.length()];
-    Arrays.fill(masked, '0');
-    char[] xx = Long.toBinaryString(value).toCharArray();
-    System.arraycopy(xx, 0, masked, masked.length - xx.length, xx.length);
-    int floatCount = 0;
-    for (int i = 0; i < mask.length(); i++) {
-      if (mask.charAt(i) != exceptionChar) {
-        masked[i] = mask.charAt(i);
-        if (masked[i] == 'X') {
-          floatCount++;
+  private long[] mask(long value, char[] mask, char exceptionChar) {
+    char[] maskedValue = toBinary(value, mask.length);
+    SortedSet<Integer> xPositions = new TreeSet<>();
+    for (int i = 0; i < mask.length; i++) {
+      if (mask[i] != exceptionChar) {
+        maskedValue[i] = mask[i];
+        if (maskedValue[i] == 'X') {
+          xPositions.add(i);
         }
       }
     }
 
-    List<Long> answers = new LinkedList<>();
-    int max = 1 << floatCount;
-    for (int i = 0; i < max; i++) {
-      char[] maskedAddress = new char[masked.length];
-      System.arraycopy(masked, 0, maskedAddress, 0, masked.length);
+    long[] answers = new long[1 << xPositions.size()];
+    char[] looper = null;
+    for (int i = 0; i < answers.length; i++) {
+      looper = toBinary(i, xPositions.size());
 
-      char[] looper = new char[floatCount];
-      if (floatCount > 0) {
-        Arrays.fill(looper, '0');
-        char[] bb = Long.toBinaryString(i).toCharArray();
-        System.arraycopy(bb, 0, looper, looper.length - bb.length, bb.length);
+      int xIndex = 0;
+      char[] maskedAddress = Arrays.copyOf(maskedValue, maskedValue.length);
+      for (int xPosition : xPositions) {
+        maskedAddress[xPosition] = looper[xIndex++];
       }
-
-      int pos = 0;
-      for (int m = 0; m < maskedAddress.length; m++) {
-        if (maskedAddress[m] == 'X') {
-          maskedAddress[m] = looper[pos++];
-        }
-      }
-      answers.add(Long.parseLong(new String(maskedAddress), 2));
+      answers[i] = Long.parseLong(new String(maskedAddress), 2);
     }
     return answers;
   }
 
   private long solve(List<String> lines, char exceptionChar) {
     Map<Long, Long> mem = new HashMap<>();
-    String mask = "";
+    char[] mask = null;
 
     for (String line : lines) {
       String[] parts = line.split("\\s+=\\s+");
       if ("mask".equals(parts[0])) {
-        mask = parts[1];
+        mask = parts[1].toCharArray();
       } else {
         long ix = Long.parseLong(parts[0].replaceAll("\\D+", ""));
         long value = Long.parseLong(parts[1]);
 
         if (exceptionChar == 'X') {
-          mask(value, mask, exceptionChar).forEach(masked -> mem.put(ix, masked));
+          Arrays.stream(mask(value, mask, exceptionChar)).forEach(masked -> mem.put(ix, masked));
         } else {
-          mask(ix, mask, exceptionChar).forEach(masked -> mem.put(masked, value));
+          Arrays.stream(mask(ix, mask, exceptionChar)).forEach(masked -> mem.put(masked, value));
         }
       }
     }
@@ -115,6 +104,14 @@ public class Puzzle14 extends PuzzleAbs {
     result = solveB(lines);
     assertEquals(3278997609887L, result);
     System.out.println(result);
+  }
+
+  private char[] toBinary(long value, int length) {
+    StringBuilder sb = new StringBuilder(Long.toBinaryString(value));
+    while (sb.length() < length) {
+      sb.insert(0, '0');
+    }
+    return sb.toString().toCharArray();
   }
 
 }

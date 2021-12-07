fb22d3a330b0f6959e0c636b9f2b7dc8a6a817e5
varienaja <varienaja@mercury>
2021-12-07 16:42:54 +0100

    Less calculation, more thinking

--- src/test/java/org/varienaja/adventofcode2021/Puzzle07.java
+++ src/test/java/org/varienaja/adventofcode2021/Puzzle07.java
@@ -3,9 +3,9 @@ package org.varienaja.adventofcode2021;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
-import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 import org.junit.Test;
 
@@ -17,45 +17,42 @@ import org.junit.Test;
  */
 public class Puzzle07 extends PuzzleAbs {
 
-  private long solve(String input, boolean complicatedFuelFormula) {
-    List<Integer> poss = Arrays.stream(input.split(",")).map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+  private long solveA(String input) {
+    List<Long> poss = Arrays.stream(input.split(",")).map(s -> Long.parseLong(s)).sorted().collect(Collectors.toList());
+    long middleElt = poss.get(poss.size() / 2);
+    // The middle element has just as many smaller number on the right as it has bigger on the left
+    // Move all crabs to this position causes the least amount of steps
 
-    IntSummaryStatistics stats = poss.stream().mapToInt(i -> i).summaryStatistics();
+    return poss.stream().mapToLong(p -> Math.abs(p - middleElt)).sum();
+  }
 
-    long minFuel = Long.MAX_VALUE;
-    for (int hpos = stats.getMin(); hpos < stats.getMax(); hpos++) {
-      int fuel = 0;
-      for (Integer p : poss) {
-        int dist = Math.abs((p - hpos));
-        if (complicatedFuelFormula) {
-          fuel += (dist * (1 + dist)) / 2;
-        } else {
-          fuel += dist;
-        }
-      }
+  private long solveB(String input) {
+    List<Long> poss = Arrays.stream(input.split(",")).map(s -> Long.parseLong(s)).collect(Collectors.toList());
+    double avg = poss.stream().mapToLong(l -> l).summaryStatistics().getAverage();
+    long candidate1 = (long)Math.floor(avg);
+    long candidate2 = (long)Math.ceil(avg);
+    // When cost is not linear but cubic, we need to take the average. Since
+    // this is a double we have to try two horizontal positions
+    // For skeptics, change candidate1 and 2 to min(poss) and max(poss)
 
-      if (fuel < minFuel) {
-        minFuel = fuel;
-      }
-    }
-
-    return minFuel;
+    return LongStream.rangeClosed(candidate1, candidate2).map(hpos -> {
+      return poss.stream().mapToLong(p -> Math.abs(p - hpos)).map(dist -> (dist * (1 + dist)) / 2).sum();
+    }).min().orElse(0L);
   }
 
   @Test
   public void testDay07() {
     String testInput = "16,1,2,0,4,2,7,1,2,14";
-    assertEquals(37, solve(testInput, false));
+    assertEquals(37, solveA(testInput));
 
     announceResultA();
     String lines = getInput().get(0);
-    long result = solve(lines, false);
+    long result = solveA(lines);
     System.out.println(result);
-    assertEquals(343605, result);
 
-    assertEquals(168, solve(testInput, true));
+    assertEquals(168, solveB(testInput));
     announceResultB();
-    result = solve(lines, true);
+    result = solveB(lines);
     System.out.println(result);
     assertEquals(96744904, result);
   }

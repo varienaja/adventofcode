570acf2238ceeaee39e99201567da6d5395fd939
varienaja <varienaja@mercury>
2021-12-12 17:46:50 +0100

    Shorter + faster

--- src/test/java/org/varienaja/adventofcode2021/Puzzle12.java
+++ src/test/java/org/varienaja/adventofcode2021/Puzzle12.java
@@ -5,10 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
@@ -20,37 +18,14 @@ import org.junit.Test;
  * @see <a href="https://adventofcode.com/2021">adventofcode.com</a>
  */
 public class Puzzle12 extends PuzzleAbs {
-
-  private boolean addOK(String path, String toAdd, boolean twiceAllowed) {
-    if (path.contains(toAdd)) {
-      if (twiceAllowed) {
-        Map<String, Integer> cave2Visits = new HashMap<>();
-        for (String s : (path + "," + toAdd).split(",")) {
-          if (!s.isEmpty() && Character.isLowerCase(s.charAt(0)) && !"start".equals(s) && !"end".equals(s)) {
-            cave2Visits.compute(s, (k, v) -> v == null ? 1 : v + 1);
-          }
-        }
-
-        long max = cave2Visits.values().stream().mapToInt(i -> i).max().orElse(0);
-        if (max == 2) { // We already visited one or more caves twice
-          return cave2Visits.values().stream().filter(i -> i == 2).count() <= 1;
-        }
-        return max <= 1;
-      } else {
-        return false;
-      }
-    } else {
-      return true;
-    }
-  }
+  private final static String START = "start";
 
   private Collection<String> increaseDepth(Collection<String> paths, List<String[]> fromTos, boolean twiceOK) {
     Collection<String> newPaths = new HashSet<>();
     boolean added = false;
 
     for (String path : paths) {
-      int ix = path.lastIndexOf(',');
-      String lastCave = path.substring(ix + 1);
+      String lastCave = path.substring(path.lastIndexOf(',') + 1);
       if ("end".equals(lastCave)) {
         newPaths.add(path); // keep finished path
       } else {
@@ -58,11 +33,19 @@ public class Puzzle12 extends PuzzleAbs {
           for (int i = 0; i < fromTo.length; i++) {
             if (fromTo[i].equals(lastCave)) {
               String to = fromTo[1 - i];
-              if (!"start".equals(to)) {
-                if (Character.isUpperCase(to.charAt(0)) || addOK(path, to, twiceOK)) {
+              if (!START.equals(to)) {
+
+                if (Character.isUpperCase(to.charAt(0)) || !path.contains(to)) {
                   newPaths.add(path + "," + to);
                   added = true;
+                } else {
+                  if (twiceOK && START.equals(path.substring(1, path.indexOf(',', 1)))) {
+                    // Mark path with ! to indicate one double visit to a lowercase cave
+                    newPaths.add("!" + path + "," + to);
+                    added = true;
+                  }
                 }
+
               }
             }
           }
@@ -78,12 +61,12 @@ public class Puzzle12 extends PuzzleAbs {
 
   private long solveA(List<String> lines) {
     List<String[]> map = lines.stream().map(line -> line.split("-")).collect(Collectors.toList());
-    return increaseDepth(Collections.singleton(",start"), map, false).size();
+    return increaseDepth(Collections.singleton("," + START), map, false).size();
   }
 
   private long solveB(List<String> lines) {
     List<String[]> map = lines.stream().map(line -> line.split("-")).collect(Collectors.toList());
-    return increaseDepth(Collections.singleton(",start"), map, true).size();
+    return increaseDepth(Collections.singleton("," + START), map, true).size();
   }
 
   @Test

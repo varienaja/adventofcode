6995c762856bb96c45b223e73e72852f6808d04e
Varienaja <varienaja@gmail.com>
2022-12-08 21:23:20 +0100

    streamier

--- src/test/java/org/varienaja/adventofcode2022/Puzzle08.java
+++ src/test/java/org/varienaja/adventofcode2022/Puzzle08.java
@@ -3,6 +3,7 @@ package org.varienaja.adventofcode2022;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 import org.varienaja.Point;
@@ -32,6 +33,33 @@ public class Puzzle08 extends PuzzleAbs {
     assertEquals(201600L, result);
   }
 
+  @Test
+  public void testA() {
+    assertEquals(21L, solveA(getTestInput()));
+  }
+
+  @Test
+  public void testB() {
+    assertEquals(8L, solveB(getTestInput()));
+  }
+
+  private long calcScenicScore(List<String> lines, Point treeHouseCandidate, long maxX, long maxY) {
+    long score = 1;
+    for (Point.Direction d : Point.Direction.values()) {
+      Point tree = new Point(treeHouseCandidate.x, treeHouseCandidate.y);
+      boolean visible = true;
+      char height = lines.get(tree.y).charAt(tree.x);
+
+      while (visible && tree.x > 0 && tree.y > 0 && tree.x < maxX && tree.y < maxY) {
+        tree = tree.add(d);
+        char hh = lines.get(tree.y).charAt(tree.x);
+        visible = hh < height;
+      }
+      score *= treeHouseCandidate.manhattanDistance(tree);
+    }
+    return score;
+  }
+
   private List<String> getTestInput() {
     return List.of( //
         "30373", //
@@ -41,78 +69,46 @@ public class Puzzle08 extends PuzzleAbs {
         "35390");
   }
 
+  private boolean isVisible(List<String> lines, Point start, long maxX, long maxY) {
+    for (Point.Direction d : Point.Direction.values()) {
+      Point tree = new Point(start.x, start.y);
+      boolean visible = true;
+      char height = lines.get(tree.y).charAt(tree.x);
+
+      while (visible && tree.x > 0 && tree.y > 0 && tree.x < maxX && tree.y < maxY) {
+        tree = tree.add(d);
+        char hh = lines.get(tree.y).charAt(tree.x);
+        if (hh < height) {
+          hh = height;
+        } else {
+          visible = false;
+        }
+      }
+      if (visible) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   private long solveA(List<String> lines) {
     int maxX = lines.size() - 1;
     int maxY = lines.get(0).length() - 1;
-    long visibleTreeCount = maxX * 2 + maxY * 2; // Trees at border are always visible
 
-    for (int x = 1; x < maxX; x++) {
-      nextTree: for (int y = 1; y < maxY; y++) {
-
-        for (Point.Direction d : Point.Direction.values()) {
-          Point tree = new Point(x, y);
-          boolean visible = true;
-          char height = lines.get(tree.y).charAt(tree.x);
-
-          while (visible && tree.x > 0 && tree.y > 0 && tree.x < maxX && tree.y < maxY) {
-            tree = tree.add(d);
-            char hh = lines.get(tree.y).charAt(tree.x);
-            if (hh < height) {
-              hh = height;
-            } else {
-              visible = false;
-            }
-          }
-          if (visible) {
-            ++visibleTreeCount;
-            continue nextTree;
-          }
-        }
-
-      }
-    }
-
-    return visibleTreeCount;
+    return IntStream.rangeClosed(0, maxX).parallel().mapToLong( //
+        x -> IntStream.rangeClosed(0, maxY).filter( //
+            y -> isVisible(lines, new Point(x, y), maxX, maxY)).count() //
+    ).sum();
   }
 
   private long solveB(List<String> lines) {
     int maxX = lines.size() - 1;
     int maxY = lines.get(0).length() - 1;
-    long maxScenic = Long.MIN_VALUE;
 
-    for (int x = 1; x < maxX; x++) {
-      for (int y = 1; y < maxY; y++) {
-
-        long score = 1;
-        for (Point.Direction d : Point.Direction.values()) {
-          Point treeHouseCandidate = new Point(x, y);
-          Point tree = new Point(x, y);
-          boolean visible = true;
-          char height = lines.get(tree.y).charAt(tree.x);
-
-          while (visible && tree.x > 0 && tree.y > 0 && tree.x < maxX && tree.y < maxY) {
-            tree = tree.add(d);
-            char hh = lines.get(tree.y).charAt(tree.x);
-            visible = hh < height;
-          }
-          score *= treeHouseCandidate.manhattanDistance(tree);
-        }
-        maxScenic = Math.max(score, maxScenic);
-
-      }
-    }
-
-    return maxScenic;
-  }
-
-  @Test
-  public void testA() {
-    assertEquals(21L, solveA(getTestInput()));
-  }
-
-  @Test
-  public void testB() {
-    assertEquals(8L, solveB(getTestInput()));
+    return IntStream.range(1, maxX).parallel().mapToLong( //
+        x -> IntStream.range(1, maxY).mapToLong( //
+            y -> calcScenicScore(lines, new Point(x, y), maxX, maxY)).max().orElseThrow() //
+    ).max().orElseThrow();
   }
 
 }

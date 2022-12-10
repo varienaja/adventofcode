bbcc06c4c3a01b854ce89bad82b60c60f72fee1d
Varienaja <varienaja@gmail.com>
2022-12-10 14:28:04 +0100

    Cleanup

--- src/test/java/org/varienaja/adventofcode2022/Puzzle10.java
+++ src/test/java/org/varienaja/adventofcode2022/Puzzle10.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -48,70 +49,39 @@ public class Puzzle10 extends PuzzleAbs {
     return List.of();
   }
 
-  private long solveA(List<String> lines) {
-    Map<Long, Long> cycle2x = new HashMap<>();
-    long cycle = 1;
-    long x = 1;
+  private Map<Integer, Integer> solve(List<String> lines) {
+    Map<Integer, Integer> cycle2x = new HashMap<>();
+    int cycle = 1;
+    int x = 1;
+    cycle2x.put(cycle, x);
 
     for (String line : lines) {
-      if ("noop".equals(line)) {
-        cycle2x.put(cycle, x);
-        ++cycle;
-        cycle2x.put(cycle, x);
-      } else if (line.startsWith("addx")) {
-        long val = Long.parseLong(line.substring(line.indexOf(' ') + 1));
-        ++cycle;
-        cycle2x.put(cycle, x);
-
-        x += val;
-        ++cycle;
-        cycle2x.put(cycle, x);
+      cycle2x.put(++cycle, x); // 'noop' or 1st cycle of 'addx'
+      if (line.startsWith("addx")) {
+        x += Integer.parseInt(line.substring(line.indexOf(' ') + 1));
+        cycle2x.put(++cycle, x);
       }
     }
 
-    long[] ixs = new long[] {
-        20, 60, 100, 140, 180, 220
-    };
-    long result = 0;
-    for (long ix : ixs) {
-      result += cycle2x.get(ix) * ix;
-    }
+    return cycle2x;
+  }
 
-    return result;
+  private long solveA(List<String> lines) {
+    Map<Integer, Integer> cycle2x = solve(lines);
+    return Stream.of(20, 60, 100, 140, 180, 220).mapToInt(c -> c * cycle2x.get(c)).sum();
   }
 
   private long solveB(List<String> lines) {
-    Map<Long, Long> cycle2x = new HashMap<>();
-    long cycle = 1;
-    long x = 1;
-
-    for (String line : lines) {
-      if ("noop".equals(line)) {
-        cycle2x.put(cycle, x);
-        ++cycle;
-        cycle2x.put(cycle, x);
-      } else if (line.startsWith("addx")) {
-        long val = Long.parseLong(line.substring(line.indexOf(' ') + 1));
-        ++cycle;
-        cycle2x.put(cycle, x);
-
-        x += val;
-        ++cycle;
-        cycle2x.put(cycle, x);
-      }
-    }
-
+    final int height = 6;
+    final int width = 40;
+    Map<Integer, Integer> cycle2x = solve(lines);
     System.out.println();
 
-    for (int r = 0; r < 6; r++) {
-      for (int c = 1; c <= 40; c++) {
-        long spritePos = cycle2x.get(r * 40L + c);
-        boolean visible = c >= spritePos && c <= spritePos + 2;
-        if (visible) {
-          System.out.print("#");
-        } else {
-          System.out.print(".");
-        }
+    for (int y = 0; y < height; y++) {
+      for (int x = 1; x <= width; x++) {
+        int spritePos = cycle2x.get(y * width + x);
+        boolean visible = x >= spritePos && x < spritePos + 3;
+        System.out.print(visible ? '#' : '.');
       }
       System.out.println();
     }
